6387ce2c6b7d055ff240f8e255de2646772fc527
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-08-11 15:07:04 -0700

    completed server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-    res.send("Backend Server successfully started")
+    res.send({ message: "Backend MERN API Server successfully started"})
 })
 //nodemon server/server
 app.listen(port, () => console.log(`Backend Server Running On PORT ${port}`));
