58eac47e710fdc2d9b94a170a2d7e91ca20b838b
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-08-18 13:00:11 -0700

    created blog post form and started basic styling

--- client/src/pages/AddBlog/index.jsx
+++ client/src/pages/AddBlog/index.jsx
@@ -1,8 +1,11 @@
+import BlogForm from "../../components/BlogForm";
+
 function AddBlog() {
   return (
     <div className="flex flex-col gap-10">
       <h1 className="text-2xl font-bold text-gray-800">Add a Blog Post</h1>
-      <hr></hr>
+      <hr />
+      <BlogForm />
     </div>
   );
 }
