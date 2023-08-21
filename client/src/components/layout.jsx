b432f5c709936aabb6559537159e809557bb754b
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-08-21 12:09:22 -0700

    added home page routing on blog form page

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
+import {useNavigate} from 'react-router-dom';
 
 function Layout(props) {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="bg-gray-800 text-white p-5">
-        <h1 className="text-4xl font-bold cursor-pointer">Bunny Blog MERN</h1>
+        <h1 className="text-4xl font-bold cursor-pointer"
+        onClick={() => {
+          
+          navigate('/');
+        }}>Bunny Blog MERN</h1>
       </div>
       <div className="p-5">{props.children}</div>
     </div>
