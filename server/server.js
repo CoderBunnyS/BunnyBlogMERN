4d38a8762ee164ea456a873414cd3bde2062000e
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-10-05 15:08:01 -0700

    added information for deploying with render

--- server/server.js
+++ server/server.js
@@ -12,5 +12,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/blogs", require("./routes/blogsRoute"));
+
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+} 
 //nodemon server/server
 app.listen(port, () => console.log(`Backend Server Running On PORT ${port}`));
