b412e4474d425976eaab93e13cf122d5bf9de3e9
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-04-30 15:13:11 -0700

    added logic so edit and delet buttons only show when a user is logged in

--- client/src/pages/Blog/index.jsx
+++ client/src/pages/Blog/index.jsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import Loader from "../../components/Loader";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Blog() {
   const [blog, setBlog] = useState();
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const navigate = useNavigate();
+  const { isAuthenticated } = useAuth0();
 
   const getData = async () => {
     try {
@@ -17,7 +19,7 @@ function Blog() {
       const response = await axios.get(`/api/blogs/${id}`);
       setBlog(response.data.data);
     } catch (error) {
-      toast.error(error.mesage);
+      toast.error(error.message);  // Corrected typo from 'mesage' to 'message'
     } finally {
       setLoading(false);
     }
@@ -39,37 +41,32 @@ function Blog() {
       setLoading(false);
     }
   };
+
   useEffect(() => {
     getData();
   }, []);
-  console.log(loading);
+
   return (
     <div className="flex flex-col gap-8">
-    {loading && <Loader />}
+      {loading && <Loader />}
       <div className="flex justify-between items-center">
-        
+        <button
+          className="btn-outlined"
+          onClick={() => navigate("/")}
+        >
+          Back
+        </button>
+      </div>
+      <div className="title">
+        <h1 className="text-3xl contained contained-content-center title">{blog?.title}</h1>
+      </div>
+      <div className="container">
+        <img src={blog?.image} className="contained object-bover rounded" alt="Blog Post" />
+      </div>
+      <p className="blogPara">{blog?.description}</p>
+      {isAuthenticated && (
         <div className="flex gap-3 buttonBox">
           <button
-            className="btn-outlined"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            Back
-          </button>
- 
-        </div>
-      </div>
-       <div className="title">
-       <h1 className="text-3xl contained contained-content-center title">{blog?.title}</h1>
-       </div>
-      <div className="container" >
-      <img src={blog?.image} className="contained object-bover rounded" />
-     
-      </div>
-      <p className=" blogPara">{blog?.description}</p>
-      <div className="flex gap-3 buttonBox">
-      <button
             className="btn-contained"
             onClick={() => navigate(`/edit-blog/${id}`)}
           >
@@ -78,7 +75,8 @@ function Blog() {
           <button className="btn-outlined" onClick={onDelete}>
             Delete
           </button>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
