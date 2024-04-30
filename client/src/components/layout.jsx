e46eb71d0e158effaf92b70d756a38a352b8c5dc
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-04-29 17:56:32 -0700

    created and styled login button

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -2,6 +2,7 @@
 import { useNavigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
+
 function Layout(props) {
   const navigate = useNavigate();
   return (
@@ -15,6 +16,15 @@ function Layout(props) {
           }}
         > Bunny&rsquo;s Solutions Engineering Blog
         </h1>
+        <button
+          className="btn-contained" id="login-button"
+          onClick={() => {
+            // Add your Auth0 login logic here
+            
+          }}
+        >
+          Login
+        </button>
       </div>
       <div className="p-5">{props.children}</div>
     </div>
