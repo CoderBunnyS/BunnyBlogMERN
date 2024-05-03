dc0adb96d19dc6baeebf4aa4f65d02eaa6365898
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-03 16:30:46 -0700

    updated package.json due to deployment error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx';
 import './index.css';
-import auth_config from './auth_config.json';
+import config from './auth_config.json';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <Auth0Provider
-      domain={auth_config.domain}
-      clientId={auth_config.clientId}
-      audience={auth_config.audience}
+      domain={config.domain}
+      clientId={config.clientId}
+      audience={config.audience}
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <App />
