ba78e4f5909971b27e77baf5d2d5353c20dc3430
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-03 16:51:10 -0700

    further updates to solve deployment error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx';
 import './index.css';
-import config from './auth_config.json';
-
+import auth_config from './auth_config.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <Auth0Provider
-      domain={config.domain}
-      clientId={config.clientId}
-      audience={config.audience}
+      domain={auth_config.domain}
+      clientId={auth_config.clientId}
+      audience={auth_config.audience}
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <App />
---
+++ scripts/createConfig.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const config = {
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    audience: process.env.AUTH0_AUDIENCE
+};
+
+fs.writeFileSync(path.join(__dirname, '../src/auth_config.json'), JSON.stringify(config, null, 2));
