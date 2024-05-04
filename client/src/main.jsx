8e25251d123fac373f8cf64ccf043266b1f514ad
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-03 19:58:30 -0700

    further updates to solve deployment error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,10 @@ import './index.css';
 import auth_config from './auth_config.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
   <React.StrictMode>
     <Auth0Provider
       domain={auth_config.domain}
       clientId={auth_config.clientId}
-      audience={auth_config.audience}
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <App />
