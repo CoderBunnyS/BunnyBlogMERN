102538662f001384fee047ce48f9a3fba5b92b1b
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-06 15:42:50 -0700

    updated server.js due to deployment error

--- server/server.js
+++ server/server.js
@@ -15,13 +15,10 @@ __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   const clientPath = path.join(__dirname, '../client/dist');
   app.use(express.static(path.join(clientPath)));
-
-    // Handle all non-API routes by returning the index.html file
-    app.get(/^(?!\/api\/).*/, (req, res) => {  // Regex to ignore /api paths
-      res.sendFile(path.join(clientPath, "index.html"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  // });
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
 } 
 //nodemon server/server
 app.listen(port, () => console.log(`Backend Server Running On PORT ${port}`));
