e7d93ef01acbdc6d33625571031c20b9706702f5
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-06 15:16:42 -0700

    updated server.js due to deployment error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  const clientPath = path.join(__dirname, '../client/dist');
+  app.use(express.static(path.join(clientPath)));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
