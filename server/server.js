bbc93f0094d97d7e6b8e3a5e90c638ec1969ea64
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-06 16:00:41 -0700

    code changes due to Auth0 error

--- server/server.js
+++ server/server.js
@@ -2,23 +2,26 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-app.use(express.json())
+app.use(express.json());
 
 require('dotenv').config();
-const dbConnect = require("./config/dbConnect")
+const dbConnect = require("./config/dbConnect");
 
 app.use("/api/blogs", require("./routes/blogsRoute"));
 
 const path = require("path");
-__dirname = path.resolve();
+// Ensure __dirname is set to the directory of the current module
+__dirname = path.resolve(path.dirname(''));
 
 if (process.env.NODE_ENV === "production") {
   const clientPath = path.join(__dirname, '../client/dist');
-  app.use(express.static(path.join(clientPath)));
+  app.use(express.static(clientPath));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    // Correctly reference the path using clientPath
+    res.sendFile(path.join(clientPath, "index.html"));
   });
-} 
+}
+
 //nodemon server/server
 app.listen(port, () => console.log(`Backend Server Running On PORT ${port}`));
